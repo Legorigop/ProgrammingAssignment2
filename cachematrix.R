d781386d949eddf4f35fba3e986904fe19a9a42f
Matthew Alejandro <91674638+Legorigop@users.noreply.github.com>
2021-09-30 22:30:45 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,31 +6,30 @@
 
 library(MASS)
 makeCacheMatrix <- function(x = matrix()){
-  Inverse <- NULL ## This is to set the inverse as NULL
-  Set <- function(y){ ## This aims to set the function to that of new
-        x <<- y
-        Inverse <<- NULL
+Inverse <- NULL ## This is to set the inverse as NULL
+Set <- function(y){ ## This aims to set the function to that of new
+      x <<- y
+      Inverse <<- NULL
                       }
-  Obtain <- function() {x} ## This is the function used to get the matrix. Hence, obtain.
-  SettingInv <-function(inverse)Inverse<<-inverse ## This essentially sets the inverse.
-  GettingInv<-function(){       
-                      inver<-ginv(x)
-                      inver%*%x ## Obtain the inverse.
+Obtain <- function() {x} ## This is the function used to get the matrix. Hence, obtain.
+SettingInv <-function(inverse)Inverse<<-inverse ## This essentially sets the inverse.
+GettingInv<-function(){       
+                   inver<-ginv(x)
+                   inver%*%x ## Obtain the inverse.
   }
-  list(Set = Set, Obtain = Obtain, SettingInv = SettingInv, getinv =  GettingInv)
+  list(Set = Set, Obtain = Obtain, SettingInv = SettingInv, getinv =  GettingInv) ## Listing the variables.
   
 }
 
 ## The following function is used to obtain and then calculate/solve the inverse of a given matrix from a chache.
-
 cacheSolve <- function(x, ...){
-  Inverse <- x$GettingInv()
-  if(!is.null(Inverse)){
-                message("Currently Obtaining Matrix From The Cached Data...") ## This is to let users know that the data is being obtained. Additionally, it's used to check if NULL.
-                return(Inverse)  ## Consequently returns the value/magnitude of the inverse.                     
+Inverse <- x$GettingInv()
+if(!is.null(Inverse)){
+              message("Currently Obtaining Matrix From The Cached Data...") ## This is to let users know that the data is being obtained. Additionally, it's used to check if NULL.
+              return(Inverse)  ## Consequently returns the value/magnitude of the inverse.                     
   }
-        mat <- x$get() ## mat function
-        Inverse <-solve(mat, ...)  ## Finally the inverse is solved.            
-        x$SettingInv(Inverse)
-        Inverse
+     mat <- x$get() ## mat function
+     Inverse <-solve(mat, ...)  ## Finally the inverse is solved.            
+     x$SettingInv(Inverse)
+      Inverse
 }
